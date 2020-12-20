c806b1b542ed432f8628a4e3b613fad2db31fc82
p4nix <p4nix@users.noreply.github.com>
2020-12-20 09:52:01 +0100

    init

---
+++ __init__.py
@@ -0,0 +1,15 @@
+from aqt import mw, gui_hooks
+
+def choose_deck():
+    config = mw.addonManager.getConfig(__name__)
+
+    did = mw.col.decks.id(config["deckname"])
+    if did is None:
+        return
+
+    mw.col.decks.select(did)
+
+    mw.col.startTimebox()
+    mw.moveToState("review")
+
+gui_hooks.main_window_did_init.append(choose_deck)
---
+++ config.md
@@ -0,0 +1,2 @@
+## deckname
+Set deckname to the deck you automatically want to switch to upon starting Anki. Simple as that.
